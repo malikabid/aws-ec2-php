954f1e4f5c8fac417a5491278799a0ffa9a7a674
ABID HUSSAIN <malikabid2006@gmail.com>
2022-09-25 08:42:50 +0530

    added form field #1

--- contact.php
+++ contact.php
@@ -1,3 +1,10 @@
 <?php
 
 echo "Please fill in the below details to get connected";
+
+?>
+
+<H1>Contact Us</H1>
+<form action="">
+    <label for="firstname">Enter your firstname</label><input type="text" name="firstname" id="fname" />
+</form>
