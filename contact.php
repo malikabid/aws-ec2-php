126f50477e934df0f4ef58c4a91bb2aa20df4e5b
ABID HUSSAIN <malikabid2006@gmail.com>
2022-09-25 16:00:46 +0530

    fix changes

--- contact.php
+++ contact.php
@@ -8,15 +8,15 @@ echo "Please fill in the below details to get connected";
 <form action="">
     <table>
         <tr>
-            <td><label for="firstname">Enter your firstname</label>: </td>
+            <td><label for="firstname">Enter your Firstname</label>: </td>
             <td><input type="text" name="firstname" id="fname" /></td>
         </tr>
         <tr>
-            <td><label for="lastname">Enter your lastname</label>:</td>
+            <td><label for="lastname">Enter your Lastname</label>:</td>
             <td><input type="text" name="lastname" id="lname" /></td>
         </tr>
         <tr>
-            <td><label for="email">Enter your email</label>:</td>
+            <td><label for="email">Enter your Email</label>:</td>
             <td><input type="email" name="email" id="email" /></td>
         </tr>
         <tr>
