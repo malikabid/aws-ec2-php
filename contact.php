cf2910c02c7a9bbe319a614fde8039c2aefa6030
ABID HUSSAIN <malikabid2006@gmail.com>
2022-09-25 15:39:04 +0530

    added more form fields

--- contact.php
+++ contact.php
@@ -6,5 +6,24 @@ echo "Please fill in the below details to get connected";
 
 <H1>Contact Us</H1>
 <form action="">
-    <label for="firstname">Enter your firstname</label><input type="text" name="firstname" id="fname" />
+    <table>
+        <tr>
+            <td><label for="firstname">Enter your firstname</label>: </td>
+            <td><input type="text" name="firstname" id="fname" /></td>
+        </tr>
+        <tr>
+            <td><label for="lastname">Enter your lastname</label>:</td>
+            <td><input type="text" name="lastname" id="lname" /></td>
+        </tr>
+        <tr>
+            <td><label for="email">Enter your email</label>:</td>
+            <td><input type="email" name="email" id="email" /></td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <input type="submit" value="Submit" />
+            </td>
+        </tr>
+    </table>
+    
 </form>
